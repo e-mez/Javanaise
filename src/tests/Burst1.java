afa89cd217a19ef2fd0407ec57d06b137e9f1164
Emeziem U <emez.uwalaka@gmail.com>
2020-10-12 09:57:57 +0200

    Added 'unexecuted' test for burst read & write

---
+++ src/tests/Burst1.java
@@ -0,0 +1,55 @@
+/***
+ * Irc class : simple implementation of a chat using JAVANAISE
+ * Contact:
+ *
+ * Authors:
+ *      Chouaib Mounaime
+ *      Emeziem Uwalaka
+ */
+
+package tests;
+
+import jvn.JvnException;
+import jvn.JvnObject;
+import jvn.JvnServerImpl;
+
+/*
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.Serializable;
+*/
+
+
+public class Burst1 {
+	/**
+	 * main method
+	 * burst test of reads and writes using dynamic proxy
+	 **/
+	public static void main(String argv[]) {
+		try {
+			Sentence_itf sentence = (Sentence) JvnProxy.newInstance(new Sentence());
+			
+			int i = 0, n = 0;
+			Random rand = new Random();
+			
+			while (true) {
+				n = rand.nextInt(2);
+				if (n == 0) {
+					sentence.read();
+				}
+				else {
+					sentence.write("burst write " + ++i);
+				}
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
+
+
+
+
